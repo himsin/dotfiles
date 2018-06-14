31890f7327702e8fbefc533c8d1591d0439254d8
himsin <himanshu.speed@gmail.com>
2018-06-15 01:41:17 +0530

    removed cred.

--- work/vpn.sh
+++ work/vpn.sh
@@ -1,1 +1,21 @@
-pulsesvc -h vpn.adtran.com -u hsingla -p Adtn2712 -U https://vpn.adtran.com/dana-na/auth/url_3/login.cgi -r Contractor
+#!/bin/bash
+
+usage() {
+	echo "Usage: ./vpn.sh <username> <password>" >&2
+}
+
+if [ "$#" -ne 2 ]; then
+	usage
+	exit 1
+fi
+if [ -z "$1" ]; then
+  echo "Username not provided. Exiting!" >&2
+  exit 1
+fi
+if [ -z "$2" ]; then
+  echo "Password not provided. Exiting!" >&2
+  exit 1
+fi
+
+echo "Connecting vpn..."
+pulsesvc -h vpn.adtran.com -u $1 -p $2 -U https://vpn.adtran.com/dana-na/auth/url_3/login.cgi -r Contractor
